7a5e7d8d41040fb32540a6e38392bf232893f669
Lesandra17 <Lesya.lisa1708@gmail.com>
2023-10-24 20:46:08 +0300

    update js1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+function createMarkup(arr) {
+  return arr
+    .map(({ preview, original, description }) => `<li class="gallery__item">
+     <a class="gallery__link" href="${original}">
+       <img
+         class="gallery__image"
+         src="${preview}"
+         alt="${description}"
+       />
+     </a>
+   </li>`)
+    .join('');
+}
+
+const galleryList = document.querySelector('.gallery');
+galleryList.insertAdjacentHTML('afterbegin', createMarkup(galleryItems));
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  close: false,
+});
