6f160d3ec4201011b6a9ede67c84397b1a292e16
Lesandra17 <Lesya.lisa1708@gmail.com>
2023-10-25 13:29:50 +0300

    update js 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'
 
+const vimeoPlayer = new Player('vimeo-player');
+
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+
+const updateCurrentTime = (time) => {
+  localStorage.setItem(LOCAL_STORAGE_KEY, time);
+};
+
+const getSavedTime = () => {
+    return Number(localStorage.getItem(LOCAL_STORAGE_KEY)) || 0;
+};
+
+vimeoPlayer.on('timeupdate', throttle((event) => {
+  const currentTime = event.seconds;
+  updateCurrentTime(currentTime);
+}, 1000));
+
+vimeoPlayer.setCurrentTime(getSavedTime());
