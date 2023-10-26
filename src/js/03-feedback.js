73f78dec13812e15d1b41e90e53a8f089d23c359
Lesandra17 <Lesya.lisa1708@gmail.com>
2023-10-26 13:03:08 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle'
 
+const feedbackForm = document.querySelector('.feedback-form');
+const emailInput = feedbackForm.querySelector('input[name="email"]');
+const messageTextarea = feedbackForm.querySelector('textarea[name="message"]');
+const feedbackStorageKey = 'feedback-form-state';
+
+const saveFormStateToLocalStorage = () => {
+  const formState = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  localStorage.setItem(feedbackStorageKey, JSON.stringify(formState));
+};
+
+const loadFormStateFromLocalStorage = () => {
+  const savedState = localStorage.getItem(feedbackStorageKey);
+  if (savedState) {
+    const formState = JSON.parse(savedState);
+    emailInput.value = formState.email;
+    messageTextarea.value = formState.message;
+  }
+};
+
+loadFormStateFromLocalStorage();
+
+emailInput.addEventListener('input', saveFormStateToLocalStorage);
+messageTextarea.addEventListener('input', saveFormStateToLocalStorage);
+feedbackForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    localStorage.removeItem(feedbackStorageKey);
+    console.log({email: emailInput.value, message: messageTextarea.value,});
+});
+    
